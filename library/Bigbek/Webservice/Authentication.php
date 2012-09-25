c25eb6c0625b45f8f3175c2abd1d1d904bc2183b
vahram <vahram@localhost>
2012-09-25 17:13:29 +0400

    FB adapter implementation

--- library/Bigbek/Webservice/Authentication.php
+++ library/Bigbek/Webservice/Authentication.php
@@ -33,7 +33,7 @@ class Authentication extends BaseWebservice
     public function fbSignUp($accessToken, $expirationDate)
     {
 	//$fb = new Facebook_User($uid, $accessToken);
-	echo \Zend_Json::encode(array('session' => 'asdfasdfasdfasdfasdfasdf'));
+	return \Zend_Json::encode(array('session' => 'asdfasdfasdfasdfasdfasdf'));
 	//return true;
     }
 
@@ -42,15 +42,15 @@ class Authentication extends BaseWebservice
      * @param string $session
      * @return boolean
      */
-    public function fbLogin($session)
+    public function signIn($session)
     {
 
 	/* var_dump($this->em->find('User', 1));
 	  $fb = new Facebook_User($uid, $accessToken); */
 	if ($session == 'asdfasdfasdfasdfasdfasdf') {
-	    echo \Zend_Json::encode(array('result' => 'success'));
+	    return \Zend_Json::encode(array('result' => 'success'));
 	} else {
-	    echo \Zend_Json::encode(array('error' => 'wrong session'));
+	    return \Zend_Json::encode(array('error' => 'wrong session'));
 	}
 	//return true;
     }
