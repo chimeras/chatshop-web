5fd4cfef86a076cfdff35e281a6a9e1d54c520a2
vahram <vahram@localhost>
2012-10-11 14:17:49 +0400

    dependency removed

--- application/models/ShoppingLists.php
+++ application/models/ShoppingLists.php
@@ -3,7 +3,7 @@
 class Application_Model_ShoppingLists extends Application_Model_Db_Table_ShoppingLists
 {
 
-	protected $_dependentTables = 'Application_Model_ShoppingListItem';
+	//protected $_dependentTables = 'Application_Model_ShoppingListItem';
 	protected $_referenceMap = array(
 		'Users' => array(
 			'columns' => array('user_id'), /* foreign key */
