20f8004178049a47f0e9e8ed929a85c01c56ae57
vahram <vahram@localhost>
2012-10-25 18:34:15 +0400

    file handling implemented

--- application/controllers/FeedController.php
+++ application/controllers/FeedController.php
@@ -11,7 +11,22 @@ class FeedController extends Zend_Controller_Action
 	{
 		$this->_helper->viewRenderer->setNoRender(true);
 		$this->_helper->layout->disableLayout();
-		$formData = $this->_request->getPost();
+		//$formData = $this->_request->getPost();
+$upload = new Zend_File_Transfer();
+$files = $upload->getFileInfo();
+//Zend_Debug::dump($files);
+
+$destinationDir = APPLICATION_PATH .'/../data/';
+foreach($files as $file){
+	echo $file['tmp_name'] ."--->";
+	echo $destinationDir.$file['name'] ."|";
+	
+	Zend_Debug::dump(move_uploaded_file($file['tmp_name'], $destinationDir .$file['name']));
+}
+
+
+$upload->receive();
+return;
 		/* @var $logger Zend_Log */
 		$logger = Zend_Registry::get('logger');
 		$logger->log(Zend_Debug::dump($formData), Zend_Log::DEBUG); 
