daf14d03df8db2d8a234d13cd998d1939973d586
vahram <vahram@localhost>
2012-11-15 21:02:58 +0400

    default category added for retailer

--- application/models/Category.php
+++ application/models/Category.php
@@ -5,6 +5,7 @@ class Application_Model_Category extends Application_Model_Db_Row_Category
 
 	public $products = array();
 	private $_subcategories = null;
+
 	/**
 	 * 
 	 * @return array(Application_Model_AdvertiserCategory)
@@ -38,7 +39,7 @@ class Application_Model_Category extends Application_Model_Db_Row_Category
 	{
 		$category = $this->toArray();
 		$category['products'] = array();
-		foreach ($this->getProducts($productsCount, $offset) as $Product){
+		foreach ($this->getProducts($productsCount, $offset) as $Product) {
 			$productArray = $Product->toArray();
 			$productArray['parent_category_id'] = $Product->parent_category_id;
 			$productArray['similar_items_count'] = $Product->getSimilarItemsCount();
@@ -51,72 +52,90 @@ class Application_Model_Category extends Application_Model_Db_Row_Category
 
 	public function getSubcategoriesArray()
 	{
-		if($this->_subcategories == null){
+		if ($this->_subcategories == null) {
 			$subs = array();
-			foreach ($this->getSubcategories() as $Sub){
-				$subs[] = array('id'=>$Sub->getId(),
-					'name' => $Sub->getName(), 
-					'products_qty'=>$Sub->getProductsCount());
+			foreach ($this->getSubcategories() as $Sub) {
+				$subs[] = array('id' => $Sub->getId(),
+					'name' => $Sub->getName(),
+					'products_qty' => $Sub->getProductsCount());
 			}
 			$this->_subcategories = $subs;
 		}
 		return $this->_subcategories;
 	}
+
 	public function getProducts($rowCount, $page)
 	{
 		$ids = array();
+		$retailersIds = $this->_getRetailersIds();
 		$subIds = array();
-		foreach($this->getAdvertiserCategories() as $cACategory){
+		foreach ($this->getAdvertiserCategories() as $cACategory) {
 			$ids[] = $cACategory->getId();
 			$subIds[$cACategory->getId()] = $this->getId();
 		}
-		
-		foreach ($this->getSubcategories() as $sub){
-			foreach($sub->getAdvertiserCategories() as $subACategory){
+
+		foreach ($this->getSubcategories() as $sub) {
+			foreach ($sub->getAdvertiserCategories() as $subACategory) {
 				$ids[] = $subACategory->getId();
 				$subIds[$subACategory->getId()] = $sub->getId();
 			}
-			
 		}
-		if(count($ids) == 0){
+		if (count($ids) == 0 || count($retailersIds)==0) {
 			return array();
 		}
 		$table = new Application_Model_Products;
 		$select = $table->select('*')
 				->group('similarity')
-				->where('`advertiser_category_id` IN('. implode(',', $ids) .')')
+				->where('`advertiser_category_id` IN(' . implode(',', $ids) . ')')
+				->where('`retailer_id` IN(' . implode(',', $retailersIds) . ')')
 				->limitPage($page, $rowCount);
 		$results = array();
-		foreach($table->fetchAll($select) as $Product){
+		foreach ($table->fetchAll($select) as $Product) {
 			$Product->parent_category_id = $subIds[$Product->getAdvertiserCategoryId()];
 			$results[] = $Product;
 		}
 		return $results;
 	}
 
-	
 	public function getProductsCount()
 	{
 		$ids = array();
-		foreach($this->getAdvertiserCategories() as $cACategory){
+		$retailersIds = $this->_getRetailersIds();
+		foreach ($this->getAdvertiserCategories() as $cACategory) {
 			$ids[] = $cACategory->getId();
 		}
-		
-		foreach ($this->getSubcategories() as $sub){
-			foreach($sub->getAdvertiserCategories() as $subACategory){
+
+		foreach ($this->getSubcategories() as $sub) {
+			foreach ($sub->getAdvertiserCategories() as $subACategory) {
 				$ids[] = $subACategory->getId();
 			}
-			
 		}
-		if(count($ids) == 0){
+		if (count($ids) == 0 || count($retailersIds)==0) {
 			return 0;
 		}
 		$table = new Application_Model_Products;
 		$select = $table->select('*')
 				->group('similarity')
-				->where('`advertiser_category_id` IN('. implode(',', $ids) .')');
+				->where('`advertiser_category_id` IN(' . implode(',', $ids) . ')')
+				->where('`retailer_id` IN(' . implode(',', $retailersIds) . ')');
 		return $table->fetchAll($select)->count();
 	}
 
-	
+	private function _getRetailersIds()
+	{
+		$Table = new Application_Model_Retailers;
+		$result = array();
+		
+		if($this->getParentId() > 0){
+			$where = 'category_id IS NULL OR category_id = '. $this->getParentId();
+		}else{
+			$where = 'category_id IS NULL OR category_id = '. $this->getId();
+		}
+		foreach ($Table->fetchAll($where) as $retailer){
+			$result[] = $retailer->getId();
+		}
+		
+		return $result;
+	}
+
 }
