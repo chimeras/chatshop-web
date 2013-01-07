a0b941c43f5c8cdde1abf81381f0272b948dc43a
Vahram Davoyan <vahram.davoyan@gmail.com>
2013-01-07 13:23:42 +0400

    category products count added to feed

--- library/Bigbek/Webservice/ShopWebservice.php
+++ library/Bigbek/Webservice/ShopWebservice.php
@@ -68,13 +68,12 @@ class ShopWebservice extends BaseWebservice
         $this->_logger->log('getThemeCategories for id=' . $id, \Zend_Log::INFO);
         $cache = \Zend_Registry::get('cache');
         $cacheID = 'theme_categories_'.$id .'_rand_'.rand(100, 101);
-      //  var_dump($cacheID);
         $categories = false; //$cache->load($cacheID);
         if ($categories === false) {
            // echo 'generating ';
             $theme = $this->_themes->fetch($id);
             $categories = $theme->getCategoriesArray();
-            $cache->save($categories); /////////////////////////////////////@@todo uncomment this to use cache
+         //   $cache->save($categories); /////////////////////////////////////@@todo uncomment this to use cache
         }else{
             shuffle($categories);
         }
