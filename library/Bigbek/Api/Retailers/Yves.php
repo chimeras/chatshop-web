cb875ae8c0196fdb21ff436f2eda241c4e07fdca
Vahram Davoyan <vahram.davoyan@gmail.com>
2013-01-24 14:15:03 +0400

    beauty bridge and dc shoes case

--- library/Bigbek/Api/Retailers/Yves.php
+++ library/Bigbek/Api/Retailers/Yves.php
@@ -7,7 +7,11 @@
 namespace Bigbek\Api\Retailers;
 class Yves extends Common
 {
-
+    public function __construct()
+    {
+        array_push($this->_blacklistKeywords, 'men');
+        array_push($this->_blacklistKeywords, 'women');
+    }
 
 
     public function connectCategoryProduct($product)
@@ -15,38 +19,26 @@ class Yves extends Common
         $connectionsTable = new \Application_Model_CategoryXProducts;
         $connectionsTable->delete('product_id=' . $product->getId());
 
+        $connection = $connectionsTable->createRow();
+        $connection->setFromArray(array(
+            'product_id' => $product->getId(),
+            'category_id' => $this->_retailer->getCategoryId(),
+            'retailer_id' => $product->getRetailerId(),
+            'brand_id' => $product->getBrandId(),
+            'type' => 2,
+            'similarity' => $product->getSimilarity()));
+        $connection->save();
+
         foreach ($this->_processor->getProcessedCategories() as $id => $category) {
-
             $type = 0;
-            $topCategoryId = $product->getTopCategoryId();
-            if ($this->_checkName($category['object']->getKeywords() . $category['parentKeywords'], $product->getName())) {
+            if ($category['object']->getParentId() > 0
+                && $this->_checkKwd($category['object']->getKeywords(), $product->getAdvertiserKeywords())){
+                $type = 4;
+            } elseif ($this->_checkName($category['object']->getKeywords(), $product->getName())){
                 $type = 2;
-            } elseif ($category['object']->getParentId() > 0
-                && $topCategoryId > 0
-                && $this->_checkKwd($category['object']->getKeywords() . $category['parentKeywords'], $product->getAdvertiserKeywords())
-            ) {
-                $type = 5;
-            } elseif ($category['object']->getParentId() > 0
-                && $topCategoryId > 0
-                && $this->_checkKwd($category['object']->getKeywords() . $category['parentKeywords'], $product->getKeywords())
-            ) {
-                $type = 1;
-            } elseif ($this->_retailer->getCategoryId() == $id) {
-                $type = 2;
-            } elseif ($category['object']->getParentId() == 0 && (
-                $this->_checkKwd($category['object']->getKeywords(), $product->getAdvertiserKeywords())
-                    || $this->_checkKwd($category['object']->getKeywords(), $product->getKeywords())
-                    || $this->_checkName($category['object']->getKeywords(), $product->getName())
-            )
-            ) {
-                if ($topCategoryId > 0) {
-                    $connectionsTable->delete("product_id=" . $product->getId() . " AND category_id=" . $topCategoryId);
-                }
-                $type = 1;
             }
-
             if ($type > 0) {
-                $connection = $connectionsTable->fetchNew();
+                $connection = $connectionsTable->createRow();
                 $connection->setFromArray(array(
                     'product_id' => $product->getId(),
                     'category_id' => $id,
@@ -55,6 +47,7 @@ class Yves extends Common
                     'type' => $type,
                     'similarity' => $product->getSimilarity()));
                 $connection->save();
+                return true;
             }
 
         }
