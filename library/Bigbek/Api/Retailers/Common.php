8c0d833fcf3e2b3c0c3eb87ef25a2a9b962dba52
Vahram Davoyan <vahram.davoyan@gmail.com>
2013-01-30 18:20:22 +0400

    common case change to new logic

--- library/Bigbek/Api/Retailers/Common.php
+++ library/Bigbek/Api/Retailers/Common.php
@@ -26,38 +26,13 @@ class Common
         $connectionsTable = new \Application_Model_CategoryXProducts;
         $connectionsTable->delete('product_id=' . $product->getId());
 
+        $type = 0;
         foreach ($this->_processor->getProcessedCategories() as $id => $category) {
-
-            $type = 0;
-            $topCategoryId = $product->getTopCategoryId();
-            if ($category['object']->getParentId() > 0
-                && $topCategoryId > 0
-                && $this->_checkKwd($category['object']->getKeywords() . $category['parentKeywords'], $product->getAdvertiserKeywords())
-            ) {
-                $type = 4;
-            } elseif ($category['object']->getParentId() > 0
-                && $topCategoryId > 0
-                && $this->_checkKwd($category['object']->getKeywords() . $category['parentKeywords'], $product->getKeywords())
-            ) {
-                $type = 3;
-            } elseif ($this->_retailer->getCategoryId() == $id) {
-                $type = 2;
-            } elseif ($category['object']->getParentId() == 0 && (
-                $this->_checkKwd($category['object']->getKeywords(), $product->getAdvertiserKeywords())
-                    || $this->_checkKwd($category['object']->getKeywords(), $product->getKeywords())
-                    || $this->_checkName($category['object']->getKeywords(), $product->getName())
-            )
-            ) {
-                if ($topCategoryId > 0) {
-                    $connectionsTable->delete("product_id=" . $product->getId() . " AND category_id=" . $topCategoryId);
-                }
+            if($category['object']->getParentId() === 0
+                && $this->_checkKwd($category['object']->getKeywords(), $product->getAdvertiserKeywords())){ // top category
                 $type = 1;
-            }elseif ($this->_checkName($category['object']->getKeywords() . $category['parentKeywords'], $product->getName())) {
-                $type = 1;
-            }
-
-            if ($type > 0) {
-                $connection = $connectionsTable->fetchNew();
+                // set top category
+                $connection = $connectionsTable->createRow();
                 $connection->setFromArray(array(
                     'product_id' => $product->getId(),
                     'category_id' => $id,
@@ -66,8 +41,24 @@ class Common
                     'type' => $type,
                     'similarity' => $product->getSimilarity()));
                 $connection->save();
-            }
 
+                foreach ($this->_processor->getProcessedCategories() as $id => $subCategory) {
+                    if($subCategory['object']->getParentId() === $category['object']->getId()
+                    && $this->_checkKwd($subCategory['object']->getKeywords(), $product->getAdvertiserKeywords())){ // category
+                        $type = 4;
+                        // set category
+                        $connection = $connectionsTable->createRow();
+                        $connection->setFromArray(array(
+                            'product_id' => $product->getId(),
+                            'category_id' => $id,
+                            'retailer_id' => $product->getRetailerId(),
+                            'brand_id' => $product->getBrandId(),
+                            'type' => $type,
+                            'similarity' => $product->getSimilarity()));
+                        $connection->save();
+                    }
+                }
+            }
         }
     }
 
