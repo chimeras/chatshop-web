7ca3984b7d763aab058fa497ae17677e2414753d
Vahram Davoyan <vahram.davoyan@gmail.com>
2013-01-30 12:21:18 +0400

    reverted working state

--- application/models/Theme.php
+++ application/models/Theme.php
@@ -36,18 +36,8 @@ class Application_Model_Theme extends Application_Model_Db_Row_Theme
 	public function getCategoriesArray()
 	{
 		$return = array();
-		foreach ($this->fetcAll() as $Category) {
+		foreach ($this->_fetchCategories(false) as $Category) {
 			$return[] = $Category->toCombinedArray(20, 0, true);
-		}
-		return $return;
-	}
-    public function getAllCategories()
-	{
-		$return = array();
-        $categoryTable = new Application_Model_Categories;
-        $Categories = $categoryTable->fetchAll();
-		foreach ($Categories as $Category) {
-			$return[] = array('id'=>$Category->getId(),'parent_id'=>$Category->getParentId(),'name'=>$Category->getName());
 		}
 		return $return;
 	}
